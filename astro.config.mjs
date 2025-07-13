eb7fe44926184cf8a70e1470ca12e444e3ae2d83
yjkwak-es <yjkwak.es@gmail.com>
2025-07-13 13:32:29 +0900

    블로그 초기 설정 및 헤더, 푸터 설정

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://your-username.github.io',
+  site: 'https://Yong-joon-Kwak.github.io',
 //   base: '/dev-blog', // GitHub Pages 배포 시 필요
   integrations: [vue(), tailwind()],
 });

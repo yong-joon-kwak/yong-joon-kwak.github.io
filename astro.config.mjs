f892bc797e5ead62fd2f9e953812af7a784f5f79
yjkwak-es <yjkwak.es@gmail.com>
2025-07-26 14:42:34 +0900

    base 수정

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://Yong-joon-Kwak.github.io',
-//   base: '/dev-blog', // GitHub Pages 배포 시 필요
+  site: 'https://yong-joon-kwak.github.io',
+  base: '/', // GitHub Pages 배포 시 필요
   integrations: [vue(), tailwind()],
 });
